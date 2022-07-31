c76595daf809511945e00de5522566e12b2ebda6
Andy Tang <winterwind2022@gmail.com>
2022-07-31 14:05:59 -0500

    create things in gamehandler

--- utils/sockets/gameHandler.tsx
+++ utils/sockets/gameHandler.tsx
@@ -11,3 +11,73 @@ export function getRandomCards(): CardType[] {
 
   return fourCards;
 }
+
+export default (io, socket) => {
+  const [cards, setCards] = useState<CardType[]>([]);
+
+  // head off hydration problem
+  useEffect(() => setCards(getRandomCards()), [])
+
+  const getCards = (msg) => {
+    socket.broadcast.emit("currentCards", cards);
+  };
+
+  const evaluateGuess = (msg) => {
+    let evaluatedGuess = verifyOperations(msg.input, cards);
+  }
+
+  socket.on("getCards", getCards);
+  socket.on("sendGuess", evaluateGuess);
+};
+
+export function verifyOperations(input: string, cards: CardType[]): string {
+  console.log("received " + input)
+  for(var i = 0; i < input.length; i++) {
+    let char = input.charAt(i);
+    if(char !== '+' && char !== '-' && char !== '*' && char !== '/' && char !== '('
+        && char !== ')' && char !== ' ' && !(char >= '0' && char <= '9')) {
+          return "invalid-Bad Character";
+    }
+  }
+
+  // Check if the string can be split for cards
+  let found = [false, false, false, false]
+  let tokens = input.split(/\D/)
+  for(var i = 0; i < tokens.length; i++) {
+    if(tokens[i] !== "") {
+      let val = parseInt(tokens[i]);
+      let ok = false;
+      for(var j = 0; j < 4; j++) {
+        if(!found[j] && cards[j].value === val) {
+          found[j] = true;
+          ok = true;
+          break;
+        }
+      }
+
+      if(!ok) {
+        return "invalid-Extra Number";
+      }
+    }
+  }
+
+  for(var i = 0; i < 4; i++) {
+    if(!found[i]) {
+      return "invalid-Missing Number";
+    }
+  }
+
+  try {
+    let val = mexp.eval(input);
+    console.log("valid and evaluated: " + val);
+    if(val === 24) {
+      return "correct"
+    }
+    else {
+      return "incorrect"
+    }
+  }
+  catch(e){
+    return "invalid-Bad Expression";
+  }
+}
