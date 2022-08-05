e951ce6669335696b831f86f3f688fdd2a043077
very <lolman7432@gmail.com>
2022-08-05 12:51:22 -0400

    temp history update

--- components/history.tsx
+++ components/history.tsx
@@ -13,34 +13,37 @@ export default function HistoryInfo(props: RoundInfo) {
   switch(props.color) {
     case 0:
       return (
-        <div className={styles.historyBox}>
-          <div className="flex flex-wrap w-full">{props.values.map((card, index) => (
-            <Card suit={card.suit} val={card.value} key={"card" + index.toString()} small={true}></Card>
-          ))}</div>
-          <p>{props.query}</p>
-          <p>{props.message}</p>
-        </div>
+          <div className="bg-green-200 p-1 rounded-md">
+              <p className="font-bold text-base text-green-900">Set #{props.setCt}</p>
+              <div className="flex flex-wrap w-full">{props.values.map((card, index) => (
+                  <Card suit={card.suit} val={card.value} key={"card" + index.toString()} small={true}></Card>
+              ))}</div>
+              <p className="text-green-500">{props.query}</p>
+              <p className="text-green-500">{props.message}</p>
+          </div>
       );
       break;
     case 1:
       return (
-        <div className={styles.historyBox}>
-          <div className="flex flex-wrap w-full">{props.values.map((card, index) => (
-            <Card suit={card.suit} val={card.value} key={"card" + index.toString()} small={true}></Card>
-          ))}</div>
-          <p className="text-red-700">{props.query}</p>
-          <p className="text-red-700">{props.message}</p>
-        </div>
+          <div className="bg-red-200 p-1 rounded-md">
+              <p className="font-bold text-base text-red-900">Set #{props.setCt}</p>
+              <div className="flex flex-wrap w-full">{props.values.map((card, index) => (
+                  <Card suit={card.suit} val={card.value} key={"card" + index.toString()} small={true}></Card>
+              ))}</div>
+              <p className="text-red-500">{props.query}</p>
+              <p className="text-red-500">{props.message}</p>
+          </div>
       );
       break;
     default:
       return (
-        <div className={styles.historyBox}>
+        <div className="bg-yellow-200 p-1 rounded-md">
+            <p className="font-bold text-base text-yellow-900">Set #{props.setCt}</p>
           <div className="flex flex-wrap w-full">{props.values.map((card, index) => (
             <Card suit={card.suit} val={card.value} key={"card" + index.toString()} small={true}></Card>
           ))}</div>
-          <p className="text-yellow-500">{props.query}</p>
-          <p className="text-yellow-500">{props.message}</p>
+          <p className="text-yellow-700">{props.query}</p>
+          <p className="text-yellow-700">{props.message}</p>
         </div>
       );
       break;
