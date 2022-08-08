e85ed6dc15398697b6d74774ef27c2f6b104ca96
Andy Tang <winterwind2022@gmail.com>
2022-08-07 22:06:28 -0500

    VERCEL HAVE YOU HEARD OF INFERRING

--- components/timer.tsx
+++ components/timer.tsx
@@ -5,7 +5,7 @@ type Props = {
     timer: number
 }
 
-export default function Timer(props: number) {
+export default function Timer(props: Props) {
   return (
     <div className={styles.timer}>
       <img className="w-8 h-8 inline-block mx-2 pb-0.5" src="/timer-icon.svg" />
