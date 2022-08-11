a5e5090fa7d1154554bd19cb359b5bb77df3c22b
Andy Tang <winterwind2022@gmail.com>
2022-08-10 21:36:24 -0500

    fix resize issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,16 +34,12 @@ function useWindowSize() {
     // only execute all the code below in client side
     if (typeof window !== 'undefined') {
       // Handler to call on window resize
-      function handleResize() {
-        // Set window width/height to state
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      }
 
       // Add event listener
-      window.addEventListener("resize", handleResize);
+      window.addEventListener("resize", setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      }));
 
       // Call handler right away so state gets updated with initial window size
       handleResize();
